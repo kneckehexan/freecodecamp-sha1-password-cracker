5f82dc6e6ba0213110c7e51bbc386c62f3e74a12
Philip Tunbjer <philip.tunbjer@gmail.com>
2022-01-21 22:51:01 +0100

    passing all tests

--- main.py
+++ main.py
@@ -10,5 +10,35 @@ cracked_password2 = password_cracker.crack_sha1_hash(
     "dcc466796201f7232b22a03781110a8871fd038c", use_salts=True)
 print(cracked_password2)
 
+#cracked_password2 = password_cracker.crack_sha1_hash(
+#    "53d8b3dc9d39f0184144674e310185e41a87ffd5", use_salts=True)
+#print(cracked_password2)
+
+#cracked_password2 = password_cracker.crack_sha1_hash(
+#    "b305921a3723cd5d70a375cd21a61e60aabb84ec")
+#print(cracked_password2)
+#
+#cracked_password2 = password_cracker.crack_sha1_hash(
+#    "c7ab388a5ebefbf4d550652f1eb4d833e5316e3e")
+#print(cracked_password2)
+#
+#cracked_password2 = password_cracker.crack_sha1_hash(
+#    "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8")
+#print(cracked_password2)
+#
+#print('\nUsing Salts\n----------------------------\n')
+#
+#cracked_password2 = password_cracker.crack_sha1_hash(
+#    "53d8b3dc9d39f0184144674e310185e41a87ffd5", use_salts=True)
+#print(cracked_password2)
+#
+#cracked_password2 = password_cracker.crack_sha1_hash(
+#    "da5a4e8cf89539e66097acd2f8af128acae2f8ae", use_salts=True)
+#print(cracked_password2)
+#
+#cracked_password2 = password_cracker.crack_sha1_hash(
+#    "ea3f62d498e3b98557f9f9cd0d905028b3b019e1", use_salts=True)
+#print(cracked_password2)
+
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,39 @@
-import hashlib
+from hashlib import sha1
 
-def crack_sha1_hash(hash):
-    
+def hashPass(arr):
+    newArr = []
+    for a in arr:
+        newArr.append(sha1(a.encode()).hexdigest())
+    return newArr
+
+def getPwd(pwdList, hashList, hash):
+    try:
+        index = hashList.index(hash)
+        return pwdList[index]
+    except ValueError:
+        return "PASSWORD NOT IN DATABASE"
+
+def crack_sha1_hash(hash, use_salts=False):
+
+    # Load the text files
+    with open("top-10000-passwords.txt", encoding='utf-8') as f:
+        passwords = f.read().splitlines()
+
+    with open("known-salts.txt", encoding='utf-8') as f:
+        salts = f.read().splitlines()
+
+    if use_salts:
+        passWithSalt = []
+        newPasswords = []
+        for s in salts:
+            for p in passwords:
+                passWithSalt.append(f'{p}{s}')
+                passWithSalt.append(f'{s}{p}')
+                newPasswords.append(p)
+                newPasswords.append(p)
+
+        hashedPass = hashPass(passWithSalt)
+        return getPwd(newPasswords, hashedPass, hash)
+    else:
+        hashedPass = hashPass(passwords)
+        return getPwd(passwords, hashedPass, hash)
